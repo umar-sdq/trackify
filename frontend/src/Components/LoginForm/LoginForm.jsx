82086684f466da8a6e72a8940d63bb43a57a4d98
Umar Siddiqui <2377328@cmontmorency.qc.ca>
2025-08-07 15:38:19 -0400

    Refactor user model and enhance signup form with validation and submission logic

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -2,7 +2,9 @@ const LoginForm = () => {
   return (
     <div>
       <h2>Username</h2>
+      <input type="text" placeholder="Username" />
       <h2>Password</h2>
+      <input type="password" placeholder="Password" />  
       <h2>Forgot Password?</h2>
       <h2>No account? <a href="/signup">Create an account.</a></h2>
       
--- frontend/src/Components/SignUpForm/SignUpForm.jsx
+++ frontend/src/Components/SignUpForm/SignUpForm.jsx
@@ -1,15 +1,61 @@
-const SignUpForm = () => {
-  return (
-    <div>
-      <h2>First name</h2>
-      <h2>Last name</h2>
-      <h2>Email address</h2>
-      <h2>Password</h2>
-      <h2>Confirm password</h2>
+import { useState } from "react";
 
-      
-    </div>
+const SignUpForm = () => {
+  const [passwordNotEqual, setPasswordNotEqual] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const entries = Object.fromEntries(fd.entries());
+    const data = {
+      ...entries
+    };
+
+    if (data.password !== data["confirm password"]) {
+      setPasswordNotEqual(true);
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:5001/api/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      const result = await response.json();
+
+      if (!response.ok) {
+        throw new Error(result.message);
+      }
+
+      setPasswordNotEqual(false);
+      alert("User created!");
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>First name</h2>
+      <input name="name" type="text" placeholder="First name" required />
+
+      <h2>Email address</h2>
+      <input name="email" type="email" placeholder="Email address" required />
+
+      <h2>Password</h2>
+      <input name="password" type="password" placeholder="Password" required />
+
+      <h2>Confirm password</h2>
+      <input name="confirm password" type="password" placeholder="Confirm password" required />
+
+      {passwordNotEqual && <p style={{ color: "red" }}>Passwords do not match.</p>}
+
+      <button type="submit">Create an account</button>
+    </form>
   );
-}
+};
 
 export default SignUpForm;
